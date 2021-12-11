ffe070fcf8ea4630f97720b152c3444610367c2c
mas0530 <marianasolayman@yahoo.com>
2021-12-11 09:42:57 -0600

    modified json and server

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
   useCreateIndex: true,
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/api"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
